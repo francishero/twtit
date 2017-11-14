432374b3687a1744d118b1fe1b640816886d8abb
Francis Miyoba <francismiyoba@gmail.com>
2017-11-14 00:42:24 -0800

    CRUD operations: createTweet,deleteTweet,updateTweet

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,7 +1,18 @@
+
+import GraphQlDate from 'graphql-date'
 import tweetResolvers from './tweet-resolver'
 
 export default {
+  // grahql when it sees a Date dataType it will look for it in schema 
+  Date: GraphQlDate,
+	// not function calls !!!
   Query: {
-    getTweets: tweetResolvers.getTweets()
+  	getTweet: tweetResolvers.getTweet,
+    getTweets: tweetResolvers.getTweets 
+  },
+  Mutation: {
+  	createTweet: tweetResolvers.createTweet,
+  	updateTweet: tweetResolvers.updateTweet,
+  	deleteTweet: tweetResolvers.deleteTweet
   }
 }
--- src/graphql/resolvers/tweet-resolver.js
+++ src/graphql/resolvers/tweet-resolver.js
@@ -1,6 +1,23 @@
 import Tweet from '../../models/Tweet'
 
 export default {
+	// get a single tweet from the database using the id 
+  getTweet: (_, { _id }) => Tweet.findById(_id),
   // this is the resolver to get all tweets from the database 
-  getTweets: () => Tweet.find({})
+  getTweets:  () => Tweet.find({}).sort({ createdAt: -1}),
+  // create a tweet using the data user passes in (args)
+  createTweet: (_, args) => Tweet.create(args),
+  // update a tweet using the tweets _id and then gather all the params 
+  updateTweet: (_, { _id, ...rest }) => Tweet.findOneAndUpdate(_id, rest, { new: true}),
+  // find a tweet by _id and delete it, return a message 
+  deleteTweet: async (_, { _id }) => {
+    try {
+      await Tweet.findOneAndRemove(_id)
+      return {
+        message: 'Tweet deleted successfully'
+      }
+    } catch(e) {
+      throw e
+    }
+  }
 }
